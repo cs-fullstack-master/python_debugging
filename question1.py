53c80a4770bdd6542026d21375c5839e4eeca18f
Kenn-CodeCrew <40698252+Kenn-CodeCrew@users.noreply.github.com>
2019-09-20 15:17:50 -0500

    Delete question1.py

--- question1.py
+++
@@ -1,5 +0,0 @@
-# Ask a user for the year they were born by calculating their age. Assuming they already had their birthday and it’s 2019 print “You are [AGE] years old”
-
-userInput = int(input("Enter the year you were born: "))
-age0fPerson = 2019-userInput
-print(f"You are " + str(ageOfPerson))
